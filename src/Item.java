50ea33bef64a5627040971b2517478ca4ad2df33
AlcidesThiago2016 <alcides_thiago@hotmail.com>
2024-08-07 22:19:20 -0300

    Create class Item.java

---
+++ src/Item.java
@@ -0,0 +1,39 @@
+public class Item {
+    private int codigo;
+    private String nome;
+    private double preco;
+
+    public Item(int codigo, String nome, double preco) {
+        this.codigo = codigo;
+        this.nome = nome;
+        this.preco = preco;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+    public void apresentar(){
+        System.out.println("Nome do item: " + nome + ", Preco: " + preco);
+    }
+}
