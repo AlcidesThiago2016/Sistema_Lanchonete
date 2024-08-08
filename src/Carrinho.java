d5045ab7d6c28433191ce69bba5c2f0f2a529d8e
AlcidesThiago2016 <alcides_thiago@hotmail.com>
2024-08-07 22:27:29 -0300

    Create class Carrinho.java

---
+++ src/Carrinho.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+
+public class Carrinho {
+
+    private ArrayList<Item> items;
+
+    public Carrinho() {
+        items = new ArrayList<>();
+    }
+
+    public void adicionarItem(Item item) {
+        items.add(item);
+    }
+
+    public void removerItem(Item item) {
+        for (int i = 0; i < items.size(); i++) {
+            if (items.get(i).getCodigo() == item.getCodigo()) {
+                System.out.println(items.get(i).getNome()+" removido com sucesso");
+                items.remove(i);
+            }
+        }
+    }
+
+    public void calcularPreco(){
+        double total = 0;
+        for (int i = 0; i < items.size(); i++) {
+            total += items.get(i).getPreco();
+        }
+        System.out.println("Total é R$: "+total);
+    }
+}
